cc3f738fdb3a30e2b2d4f5151a23b28891f5ffe6
aneesrehman89 <miaan.anees@gmail.com>
2025-03-14 10:50:22 +0500

    setting login request

--- Frontend/src/components/LoginPg.js
+++ Frontend/src/components/LoginPg.js
@@ -24,7 +24,7 @@ const LoginPg = () => {
     const { email, password } = data;
     
     try {
-      const LoginData = await fetch("${process.env.REACT_APP_API_URL}/login", {
+      const LoginData = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: "POST",
         headers: {"Content-Type": "application/json",},
         body: JSON.stringify({email, password}),
